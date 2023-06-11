ca3151001809e0370af5512b2bd066795f9547d4
fauzandp <fauzan27dp@gmail.com>
2023-06-11 11:19:42 +0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Fauzan Dwi Prasetyo on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
